158679a25e8acdf15a538f4618228e36292437cd
gabogg_awewe <gabogg2004@gmail.com>
2024-12-25 23:37:42 -0600

    afd2

--- .vercel/output/_functions/chunks/Shape_Dh0QIH2F.mjs
+++
@@ -1,9 +0,0 @@
-import { c as createComponent, r as renderTemplate, d as renderComponent } from './astro/server_RuF1lmQT.mjs';
-import 'kleur/colors';
-import { $ as $$Image } from './_astro_assets_BlX11tkN.mjs';
-
-const $$Shape = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate`${renderComponent($$result, "Image", $$Image, { "class": "floating-bubble-1 absolute right-0 top-0", "src": "/images/floating-bubble-1.png", "alt": "", "height": 98, "width": 103 })} ${renderComponent($$result, "Image", $$Image, { "class": "floating-bubble-2 absolute left-0 top-[387px] -z-[1]", "src": "/images/floating-bubble-2.png", "alt": "", "height": 66, "width": 33 })} ${renderComponent($$result, "Image", $$Image, { "class": "floating-bubble-3 absolute right-0 top-[605px] -z-[1]", "src": "/images/floating-bubble-3.png", "alt": "", "height": 99, "width": 50 })}`;
-}, "C:/Users/gabog/OneDrive/TEC/TEC/carrera/tercer semestre/PAGINA MEDITEC/Meditec/src/layouts/components/Shape.astro", void 0);
-
-export { $$Shape as $ };
